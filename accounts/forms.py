40fd18869a76db76ca953d24a6a912e96ed92f6c
thiagoarvore <137798387+thiagoarvore@users.noreply.github.com>
2024-05-29 14:04:30 -0300

    images

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,12 +17,25 @@ class SignUpForm(UserCreationForm):
         self.fields['email'].label = 'Endereço de e-mail'
         self.fields['password1'].label = 'Senha'
         self.fields['password2'].label = 'Confirmação de senha'
-        self.error_messages['password_mismatch'] = _("As senhas não coincidem.")
-        self.error_messages['duplicate_username'] = _("Este nome de usuário já está em uso.")
-        self.error_messages['invalid_username'] = _("O nome de usuário deve conter apenas letras, números e @/./+/-/_ caracteres.")
-        self.error_messages['password_too_short'] = _("A senha é muito curta.")
-        self.error_messages['password_too_common'] = _("Esta senha é muito comum.")
-        self.error_messages['password_entirely_numeric'] = _("A senha não pode ser completamente numérica.")
+        self.fields['username'].error_messages.update({
+            'required': _("Este campo é obrigatório."),
+            'unique': _("Este nome de usuário já está em uso."),
+            'invalid': _("O nome de usuário não deve conter espaços, deve conter apenas letras, números e @/./+/-/_ caracteres."),
+        })
+        self.fields['email'].error_messages.update({
+            'required': _("Este campo é obrigatório."),
+            'invalid': _("Insira um endereço de e-mail válido."),
+        })
+        self.fields['password1'].error_messages.update({
+            'required': _("Este campo é obrigatório."),
+            'password_too_short': _("A senha é muito curta."),
+            'password_too_common': _("Esta senha é muito comum."),
+            'password_entirely_numeric': _("A senha não pode ser completamente numérica."),
+        })
+        self.fields['password2'].error_messages.update({
+            'required': _("Este campo é obrigatório."),
+            'password_mismatch': _("As senhas não coincidem."),
+        })
         self.help_texts = {
             'username': _('Este será seu nome de usuário único, e será apenas utilizado para login.'),
             'password1': _(''),
